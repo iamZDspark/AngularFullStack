61ce3b2452ce1a3c127e70200a1a1593298cfa94
Spark <50664949+iamZDspark@users.noreply.github.com>
2020-08-27 11:18:13 +0530

    Assignment2

---
+++ Assignment2/Author.js
@@ -0,0 +1,89 @@
+var Author ={
+
+Id:1,
+Name:"swati",
+Biography:"female",
+Birthdate:"",
+Email:"Swatbhagtani178@gmail.com",
+PhotographUrl:"adgfcgc",
+Book: new Array(),
+};
+console.log(Author);
+
+var AuthorManagerObject={
+
+addAuthor: function(){
+    console.log();
+},
+
+getAllAuthors: function(){
+    console.log();
+},
+
+getAuthorById: function() {
+    console.log();
+},
+
+SearchByName: function(){
+    console.log();
+},
+SearchByBookWritten: function() {
+    console.log();
+},
+SearchBybiography: function() {
+    console.log();
+},
+DeleteAuthor: function(){
+    console.log();
+},
+UpdateAuthorDetails: function(){
+
+    console.log();
+}
+
+};
+
+var Book = {
+
+Id:1,
+Title:"Wings of Fire",
+CoverPageUrl:"khhjj",
+Author:"APJ Abdul Kalam",
+Price:900,
+Rating:5,
+
+};
+console.log(Book);
+
+var BookManager={
+    AddBook: function(){
+    console.log();
+},
+getAllBooks: function() {
+    console.log();
+},
+GeTBookById: function() {
+    console.log();
+},
+GetBookByAuthor: function(){
+    console.log();
+},
+SearchOnTitle: function() {
+    console.log();
+},
+SearchInPriceRange: function() {
+    console.log();
+},
+SearchInReviewRange: function(){
+    console.log();
+},
+DeleteBook: function(){
+    console.log();
+},
+UpdateBook: function(){
+    console.log();
+}
+};
+
+
+
